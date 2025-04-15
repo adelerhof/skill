e40f026bde21edae167a19786ba5da423ca62cd7
Erik Adelerhof <erik.adelerhof@kpn.com>
2025-04-15 18:40:04 +0200

    Add python healthcheck

---
+++ health_check/health_check.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+port = os.getenv('PORT', '5000')
+url = f"http://localhost:{port}/api/health"
+
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        exit(0)
+    else:
+        exit(1)
+except requests.exceptions.RequestException:
+    exit(1)
